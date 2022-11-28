476447b592d021bf48ee8ddf144b374b3fbf1872
Ken Ramirez <kenramirezp@gmail.com>
2022-11-27 20:54:39 -0500

    implementación de form de contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,52 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import {
+  NgModule,
+  CUSTOM_ELEMENTS_SCHEMA,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './views/index/index.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatInputModule} from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
 import { FormularioComponent } from './views/formulario/formulario.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { CuadrosComponent } from './views/cuadros/cuadros.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { MensajeComponent } from './views/mensaje/mensaje.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSidenavModule} from '@angular/material/sidenav';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
-
   {
-    path: '', redirectTo: '#', pathMatch: 'full'
-
+    path: '',
+    redirectTo: '#',
+    pathMatch: 'full',
   },
 
   {
-    path: 'index', component: IndexComponent
-
+    path: 'index',
+    component: IndexComponent,
   },
 
   {
-    path: 'mensaje', component: MensajeComponent
-
+    path: 'mensaje',
+    component: MensajeComponent,
   },
 
   {
-    path: '**', component: IndexComponent
-
-  }
-
-
-]
+    path: '**',
+    component: IndexComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -53,7 +55,6 @@ const routes: Routes = [
     FormularioComponent,
     CuadrosComponent,
     MensajeComponent,
-
   ],
   imports: [
     BrowserModule,
@@ -68,16 +69,16 @@ const routes: Routes = [
     ReactiveFormsModule,
     MatSelectModule,
     MatSidenavModule,
-    RouterModule.forRoot(routes)
-
-
+    RouterModule.forRoot(routes),
+    HttpClientModule,
   ],
 
   providers: [
-    {provide: LocationStrategy, useClass: HashLocationStrategy},
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    HttpClientModule,
   ],
 
   bootstrap: [AppComponent],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/views/formulario/formulario.component.ts
+++ src/app/views/formulario/formulario.component.ts
@@ -1,12 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormGroupDirective,
+  NgForm,
+  FormBuilder,
+} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-
+import { Router } from '@angular/router';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
   }
 }
 
@@ -18,35 +33,90 @@ interface Food {
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent implements OnInit {
-
-
   foods: Food[] = [
-    {value: 'Estrategia de marca-0', viewValue: 'Estrategia de marca'},
-    {value: 'Branding-1', viewValue: 'Branding'},
-    {value: 'Gestión de redes sociales-2', viewValue: 'Gestión de redes sociales '},
-    {value: 'Digitalización-3', viewValue: 'Digitalización'}
+    { value: 'Estrategia de marca-0', viewValue: 'Estrategia de marca' },
+    { value: 'Branding-1', viewValue: 'Branding' },
+    {
+      value: 'Gestión de redes sociales-2',
+      viewValue: 'Gestión de redes sociales ',
+    },
+    { value: 'Digitalización-3', viewValue: 'Digitalización' },
   ];
 
-  miFormulario = new FormGroup({
-    nombre: new FormControl('', Validators.required),
-    celular: new FormControl('', Validators.required),
-    apellido: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    empresa: new FormControl('', Validators.required),
+  formLoading: boolean = false;
 
-
+  contactForm = this.builder.group({
+    nombre: ['', Validators.required],
+    celular: ['', Validators.required],
+    apellido: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    empresa: ['', Validators.required],
+    servicio: ['', Validators.required],
+    mensaje: ['', Validators.required],
   });
 
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-
-  matcher = new MyErrorStateMatcher();
-
-  constructor() { }
-
-  ngOnInit(): void {
+  keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+    let inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
   }
 
+  onSubmit() {
+    if (this.formLoading) return;
+    if (!this.contactForm.valid) return;
+
+    const contactData = {
+      nombre: this.contactForm.value.nombre,
+      celular: this.contactForm.value.celular,
+      apellido: this.contactForm.value.apellido,
+      email: this.contactForm.value.email,
+      empresa: this.contactForm.value.empresa,
+      servicio: this.contactForm.value.servicio,
+      mensaje: this.contactForm.value.mensaje,
+    };
+
+    this.formLoading = true;
+    this.contactForm.disable();
+
+    this.http
+      .post<any>('https://esedor.com/send-mail-pointt.php', contactData)
+      .subscribe(
+        (resp) => {
+          this.formLoading = false;
+          this.resetForm();
+          this.contactForm.enable();
+          this.router.navigate(['/mensaje']);
+        },
+        (error) => {
+          alert(
+            'Ocurrió un error. Por favor, inténtelo nuevamente en unos minutos.'
+          );
+          this.formLoading = false;
+          this.contactForm.enable();
+        }
+      );
+  }
+
+  resetForm() {
+    this.contactForm.controls['nombre'].patchValue('');
+    this.contactForm.controls['celular'].patchValue('');
+    this.contactForm.controls['apellido'].patchValue('');
+    this.contactForm.controls['email'].patchValue('');
+    this.contactForm.controls['empresa'].patchValue('');
+    this.contactForm.controls['servicio'].patchValue('');
+    this.contactForm.controls['mensaje'].patchValue('');
+  }
+
+  constructor(
+    private http: HttpClient,
+    private builder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
 }
